4014b44a2a9244fa3b25038d4ea7cceeafc6d571
“Riskallah9” <riskallah9housni@gmail.com>
2023-08-07 06:10:55 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-			len++
+			len++;
 	}
 
-	fd = open(filename,O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content,len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if(fd == -1)
+	if (fd == -1) || w == -1)
 		return (-1);
 
 	close(fd);
