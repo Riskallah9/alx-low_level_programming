2e87b87d24cbce946d9b8921c65dcfc8e9d16caa
“Riskallah9” <riskallah9housni@gmail.com>
2024-01-16 03:40:47 -0800

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -fPIC -c *.c && gcc -shared -o liball.so *.o
+gcc -Wall -Wextra -Werorr -pedantic -c -fPIC * .c
+gcc -shared -o liball.so * .o
+export LD.LIBRARY_PATH=.:$LD_LIBRARY_PATH
